82bc2541d470e6ef94c101f9a7c1ed5cd1881259
oleg khrulev <linqur@gmail.com>
2022-03-26 15:38:36 +0300

    add 16.3

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -7,6 +7,7 @@ use App\Entity\Conference;
 use App\Form\CommentFormType;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
+use App\SpamChecker;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
@@ -35,7 +36,7 @@ class ConferenceController extends AbstractController
     }
     
     #[Route('/conference/{slug}.html', name: 'conference')]
-    public function show(Request $request, Conference $conference, CommentRepository $commentRepository, string $photoDir): Response
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository, SpamChecker $spamChecker, string $photoDir): Response
     {
         $comment = new Comment();
         $form = $this->createForm(CommentFormType::class, $comment);
@@ -56,6 +57,18 @@ class ConferenceController extends AbstractController
             }
 
             $this->entityManager->persist($comment);
+
+            $context = [
+                'user_ip' => $request->getClientIp(),
+                'user_agent' => $request->headers->get('user-agent'),
+                'referrer' => $request->headers->get('referer'),
+                'permalink' => $request->getUri(),
+            ];
+
+            if (2 === $spamChecker->getSpamScore($comment, $context)) {
+                throw new \RuntimeException('Чувак, это спамчик!');
+            }
+
             $this->entityManager->flush();
 
             return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
--- src/SpamChecker.php
+++ src/SpamChecker.php
@@ -13,37 +13,36 @@ class SpamChecker
     public function __construct(HttpClientInterface $client, string $akismentKey)
     {
         $this->client = $client;
-        $this->endpoint = sprintf('http:://localhost/spamchecker?key=%s', $akismentKey);
+        $this->endpoint = sprintf('http://oleg.korzill.ru/spamchecker/?key=%s', $akismentKey);
     }
 
     public function getSpamScore(Comment $comment, array $context): int
     {
         $response = $this->client->request('POST', $this->endpoint, [
             'body' => array_merge($context, [
-                'blog' => 'https://guestbook.example.com',
+                'blog' => 'http://127.0.0.1:8000',
                 'comment_type' => 'comment',
                 'comment_author' => $comment->getAuthor(),
                 'comment_author_email' => $comment->getEmail(),
                 'comment_content' => $comment->getText(),
                 'comment_date_gmt' => $comment->getCreatedAt()->format('c'),
-                'blog_lang' => 'ru',
+                'blog_lang' => 'en',
                 'blog_charset' => 'UTF-8',
                 'is_test' => true,
-            ])
+            ]),
         ]);
-
 
         $headers = $response->getHeaders();
 
-        if ('discard' === ($headers['x-akismet-pro-tip'][0] ?? '')) {
+        if ('discard' === ($headers['spamvalue'][0] ?? '')) {
             return 2;
         }
 
         $content = $response->getContent();
-        if (isset($headers['x-akismet-debug-help'][0])) {
-            throw new \RuntimeException(sprintf('Unable to check for spam: %s (%s).', $content, $headers['x-akismet-debug-help'][0]));
+        if (isset($headers['spamvalue'][0])) {
+            throw new \RuntimeException(sprintf('Unable to check for spam: %s (%s).', $content, $headers['spamvalue'][0]));
         }
 
-        return 0;
+        return 'true' === $content ? 1 : 0;
     }
 }
