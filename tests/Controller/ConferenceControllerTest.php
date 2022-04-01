5cc75217ca8a13b34a1b2fbc0b750772db0346c7
oleg khrulev <linqur@gmail.com>
2022-04-01 17:34:50 +0300

    add 17.7

--- tests/Controller/ConferenceControllerTest.php
+++ tests/Controller/ConferenceControllerTest.php
@@ -6,12 +6,26 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ConferenceControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function testIndex(): void
     {
         $client = static::createClient();
         $client->request('GET', '/');
 
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2', 'Give your feedback');
+    }
+
+    public function testConferencePage(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertCount(2, $crawler->filter('h4'));
+        $client->clickLink('view');
+
+        $this->assertPageTitleContains('Amsterdam');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Amsterdam 2019');
+        $this->assertSelectorExists('div:contains("There are 1 comments")');
     }
 }
