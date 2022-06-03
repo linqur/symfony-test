9e484da3c49da57a9d6d524ed4b32376890bedc0
oleg khrulev <linqur@gmail.com>
2022-06-03 13:37:21 +0300

    Добавил шаг 22.4

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,7 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.csss';
+import './styles/app.scss';
 import 'bootstrap';
 import bsCustomFileInput from 'bs-custom-file-input';
 
